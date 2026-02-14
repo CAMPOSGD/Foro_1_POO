ce3782e32d3caa04b3afd75b28e32f55031c3add
Gabriel Campos <gabrieljcampos@live.com>
2026-02-14 17:44:45 -0600

    Edicion de Angel, Final

--- src/Ejemplo_List.java
+++ src/Ejemplo_List.java
@@ -1,43 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Ejemplo_List { //
+public class Ejemplo_List {
 
     public static void main(String[] args) {
 
-        //            . ¿Cómo se declaran?
-        // 1. DECLARACIÓN E INSTANCIACIÓN (Crear la Lista)
+        // 1. DECLARACIÓN
         // -------------------------------------------------
-        // Usamos la interfaz 'List' y la implementación 'ArrayList'
-        List<String> mochila = new ArrayList<>();
+        List<String> mochila =new ArrayList<>();
+        // 2. ASIGNAR VALORES (.add)
+        // -------------------------------------------------
+        mochila.add("TABLET");
+        mochila.add("TAREA");
+        mochila.add("CAFE");
 
-        //      ¿Cómo se pueden asignar valores?
-        // 2. ASIGNAR VALORES (Agregar con .add)
-        // -------------------------------------------------
-        mochila.add("Tablet"); // Índice 0
-        mochila.add("Cuaderno de Apuntes"); // Índice 1
-        mochila.add("Botella de Agua");    // Índice 2
 
-        //   ¿Cómo se pueden eliminar valores?
-        // 3. MOSTRAR TAMAÑO Y DATOS
+        // 3. MOSTRAR TAMAÑO
         // -------------------------------------------------
-        System.out.println("---- INVENTARIO INICIAL ----");
-        // Usamos .size() en lugar de .length
-        System.out.println("Elementos en la mochila: " + mochila.size());
+        System.out.println("Elementos: " + mochila.size()); // .size()
 
-        // Usamos .get(0) en lugar de [0]
-        System.out.println("El primer objeto es: " + mochila.get(0));
-
+        // 4. ELIMINAR VALORES (.remove)
         // -------------------------------------------------
-        // 4. ELIMINAR VALORES (Con .remove)
+        mochila.remove("TAREA");
+        // 5. RECORRER
         // -------------------------------------------------
-        System.out.println("\n... Sacando el Cuaderno ...");
-        mochila.remove("Cuaderno de Apuntes");
-
-        // -------------------------------------------------
-        // 5. RECORRER LA LISTA FINAL
-        // -------------------------------------------------
-        System.out.println("---- INVENTARIO ACTUALIZADO ----");
         for (String objeto : mochila) {
             System.out.println("- " + objeto);
         }
